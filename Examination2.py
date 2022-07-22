643a62f2040bb68ee621c8eb6801fb490548afbc
femalecoda <odiazainab@gmail.com>
2022-07-22 01:08:16 +0100

    Examination 2

---
+++ Examination2.py
@@ -0,0 +1,9 @@
+#Declare a varible called name and age
+#what age will you be in 100 years
+
+
+name = input("enter your name")
+age = int (input("enter you current age"))
+birth_year = 2022 - age
+future_year = birth_year + 100
+print(name, " will be 100 years in year", future_year)
